379714520cd8f2a35ba4786cecde0105b228142f
Le Berre <brice.le-berre@epitech.eu>
2022-02-24 17:18:37 +0100

    feat: test authenticate middleware

--- internal/api/application/middleware_test.go
+++ internal/api/application/middleware_test.go
@@ -6,10 +6,16 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/brice-74/golang-base-api/internal/api/application"
+	"github.com/brice-74/golang-base-api/internal/domains/user"
+	"github.com/brice-74/golang-base-api/internal/testutils"
+	"github.com/brice-74/golang-base-api/internal/testutils/factory"
 	"github.com/brice-74/golang-base-api/pkg/jsonlog"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/google/go-cmp/cmp"
+	"github.com/twinj/uuid"
 )
 
 func TestEnableCORS(t *testing.T) {
@@ -146,5 +152,243 @@ func TestRateLimit(t *testing.T) {
 				t.Errorf("response code http must be 200 at request %d, got %d", i, res.Code)
 			}
 		}
+	}
+}
+
+func TestAuthenticate(t *testing.T) {
+	var (
+		db  = testutils.PrepareDB(t)
+		fac = factory.New(t, db)
+		app = &application.Application{
+			Logger: jsonlog.New(
+				os.Stdout,
+				jsonlog.LevelDisable,
+				jsonlog.Middlewares{},
+			),
+			Models: application.NewModels(db),
+		}
+	)
+
+	app.Config.JWT.Access.Secret = "secret"
+
+	var createToken = func(t *testing.T, method *jwt.SigningMethodHMAC, claims jwt.MapClaims, secret string) string {
+		to := jwt.NewWithClaims(method, claims)
+		toStr, err := to.SignedString([]byte(secret))
+		if err != nil {
+			t.Fatalf("error during jwt token creation: %s", err.Error())
+		}
+		return toStr
+	}
+
+	u := fac.CreateUserAccount(nil)
+	s := fac.CreateUserSession(&user.Session{
+		UserID: u.ID,
+	})
+
+	goodClaims := jwt.MapClaims{
+		string(application.SessionIdClaim): s.ID,
+		string(application.UserIdClaim):    u.ID,
+		string(application.ExpireClaim):    time.Now().Add(time.Minute * 3).Unix(),
+	}
+
+	expClaims := jwt.MapClaims{
+		string(application.SessionIdClaim): s.ID,
+		string(application.UserIdClaim):    u.ID,
+		string(application.ExpireClaim):    time.Time{}.Unix(),
+	}
+
+	randomIdsClaims := jwt.MapClaims{
+		string(application.SessionIdClaim): uuid.NewV4().String(),
+		string(application.UserIdClaim):    uuid.NewV4().String(),
+		string(application.ExpireClaim):    time.Now().Add(time.Minute * 3).Unix(),
+	}
+
+	badIdsClaims := jwt.MapClaims{
+		string(application.SessionIdClaim): "",
+		string(application.UserIdClaim):    "",
+		string(application.ExpireClaim):    time.Now().Add(time.Minute * 3).Unix(),
+	}
+
+	a := &application.Agent{
+		IP:    "0.0.0.0",
+		Agent: "agent",
+	}
+
+	type ExpectError struct {
+		code int
+		json string
+	}
+
+	tests := []struct {
+		title           string
+		headers         map[string]string
+		expectedHeaders map[string]string
+		expectContext   *application.ClientCtx
+		expectError     *ExpectError
+	}{
+		{
+			title: "should return context with session",
+			headers: map[string]string{
+				"Authorization": "Bearer " + createToken(t, jwt.SigningMethodHS256, goodClaims, app.Config.JWT.Access.Secret),
+			},
+			expectedHeaders: map[string]string{
+				"Vary": "Authorization",
+			},
+			expectContext: &application.ClientCtx{
+				User:    u,
+				Session: s,
+				Agent:   a,
+			},
+		},
+		{
+			title: "should return context with anonymous user",
+			expectedHeaders: map[string]string{
+				"Vary": "Authorization",
+			},
+			expectContext: &application.ClientCtx{
+				User:  user.AnonymousUser,
+				Agent: a,
+			},
+		},
+		{
+			title: "should return invalid or missing authentication token",
+			headers: map[string]string{
+				"Authorization": "bad bearer",
+			},
+			expectedHeaders: map[string]string{
+				"Vary":          "Authorization",
+				"Authorization": "Bearer",
+			},
+			expectError: &ExpectError{
+				code: 401,
+				json: `{"error":"invalid or missing authentication token"}`,
+			},
+		},
+		{
+			title: "should return unexpected signing method",
+			headers: map[string]string{
+				"Authorization": "Bearer " + createToken(t, jwt.SigningMethodHS384, goodClaims, app.Config.JWT.Access.Secret),
+			},
+			expectedHeaders: map[string]string{
+				"Vary":          "Authorization",
+				"Authorization": "Bearer",
+			},
+			expectError: &ExpectError{
+				code: 401,
+				json: `{"error":"unexpected signing method: HS384"}`,
+			},
+		},
+		{
+			title: "should return expired token",
+			headers: map[string]string{
+				"Authorization": "Bearer " + createToken(t, jwt.SigningMethodHS256, expClaims, app.Config.JWT.Access.Secret),
+			},
+			expectedHeaders: map[string]string{
+				"Vary":          "Authorization",
+				"Authorization": "Bearer",
+			},
+			expectError: &ExpectError{
+				code: 401,
+				json: `{"error":"Token is expired"}`,
+			},
+		},
+		{
+			title: "should return invalid signature token",
+			headers: map[string]string{
+				"Authorization": "Bearer " + createToken(t, jwt.SigningMethodHS256, nil, "bad secret"),
+			},
+			expectedHeaders: map[string]string{
+				"Vary":          "Authorization",
+				"Authorization": "Bearer",
+			},
+			expectError: &ExpectError{
+				code: 401,
+				json: `{"error":"signature is invalid"}`,
+			},
+		},
+		{
+			title: "should return claims not found from token",
+			headers: map[string]string{
+				"Authorization": "Bearer " + createToken(t, jwt.SigningMethodHS256, nil, app.Config.JWT.Access.Secret),
+			},
+			expectedHeaders: map[string]string{
+				"Vary":          "Authorization",
+				"Authorization": "Bearer",
+			},
+			expectError: &ExpectError{
+				code: 401,
+				json: `{"error":"Required claims from token not found"}`,
+			},
+		},
+		{
+			title: "should return not found",
+			headers: map[string]string{
+				"Authorization": "Bearer " + createToken(t, jwt.SigningMethodHS256, randomIdsClaims, app.Config.JWT.Access.Secret),
+			},
+			expectedHeaders: map[string]string{
+				"Vary": "Authorization",
+			},
+			expectError: &ExpectError{
+				code: 404,
+				json: `{"error":"User or user session not found"}`,
+			},
+		},
+		{
+			title: "should return server error",
+			headers: map[string]string{
+				"Authorization": "Bearer " + createToken(t, jwt.SigningMethodHS256, badIdsClaims, app.Config.JWT.Access.Secret),
+			},
+			expectedHeaders: map[string]string{
+				"Vary": "Authorization",
+			},
+			expectError: &ExpectError{
+				code: 500,
+				json: `{"error":"the server encountered a problem and could not process your request"}`,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.title, func(t *testing.T) {
+			next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				gotCtx := app.ClientFromContext(r.Context())
+
+				if diff := cmp.Diff(tt.expectContext, gotCtx); diff != "" {
+					t.Errorf(diff)
+				}
+			})
+
+			req := httptest.NewRequest("GET", "/", nil)
+			// fix request addr and agent
+			req.RemoteAddr = a.IP
+			req.Header.Set("User-Agent", a.Agent)
+
+			for k, v := range tt.headers {
+				req.Header.Add(k, v)
+			}
+
+			res := httptest.NewRecorder()
+			app.Authenticate(next).ServeHTTP(res, req)
+
+			if len(tt.expectedHeaders) > 0 {
+				for k, v := range tt.expectedHeaders {
+					if got := res.Header().Get(k); got != v {
+						t.Errorf("got header value %s with value %s, expected %s", k, got, v)
+					}
+				}
+			}
+
+			if tt.expectError != nil {
+				resErr := strings.TrimSpace(res.Body.String())
+				if tt.expectError.code != res.Code || tt.expectError.json != resErr {
+					t.Errorf("got code: %d and error: %s \n expect code: %d and error: %s",
+						res.Code,
+						resErr,
+						tt.expectError.code,
+						tt.expectError.json,
+					)
+				}
+			}
+		})
 	}
 }
