912fbac05ce67eb1ea8ebf350c504aa016ae5b43
Le Berre <brice.le-berre@epitech.eu>
2022-02-27 20:21:00 +0100

    feat: utils to test GQL error

--- internal/testutils/graphql.go
+++ internal/testutils/graphql.go
@@ -1,10 +1,14 @@
 package testutils
 
 import (
+	"testing"
+
 	"github.com/brice-74/golang-base-api/internal/api/application"
 	"github.com/brice-74/golang-base-api/internal/api/resolvers"
 	"github.com/brice-74/golang-base-api/internal/api/schema"
+	"github.com/google/go-cmp/cmp"
 	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/errors"
 )
 
 func ParseTestSchema(app *application.Application) *graphql.Schema {
@@ -14,4 +18,24 @@ func ParseTestSchema(app *application.Application) *graphql.Schema {
 			App: app,
 		},
 	)
+}
+
+func TestGqlError(t *testing.T, qerr *errors.QueryError, expect *ExpectResolverError) {
+	if qerr != nil {
+		if qerr.Message != expect.Msg {
+			t.Errorf("expect query error message: %s, got: %s", expect.Msg, qerr.Message)
+		}
+		if expect.Extensions != nil {
+			if diff := cmp.Diff(expect.Extensions, qerr.Extensions); diff != "" {
+				t.Error(diff)
+			}
+		}
+	} else {
+		t.Fatal("resolver error expected but not found")
+	}
+}
+
+type ExpectResolverError struct {
+	Msg        string
+	Extensions map[string]interface{}
 }
